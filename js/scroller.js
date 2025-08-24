e9efaf6a5b3e4a58c3f0839dbf276e50632dd71e
Davzrx <david.zamora@correounivalle.edu.co>
2025-08-23 23:24:20 -0500

    Estructura inicial

---
+++ js/scroller.js
@@ -0,0 +1,4 @@
+// scroller.js
+// Aquí irá la lógica del scrollytelling (interacción con scroll)
+
+console.log("scroller.js cargado correctamente");
---
+++ js/sections.js
@@ -0,0 +1,4 @@
+// sections.js
+// Aquí irán las secciones narrativas y su interacción con D3
+
+console.log("sections.js cargado correctamente");
